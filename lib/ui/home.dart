64454f737af8725c02e82cbec8fbf2eb9fee7ad0
shanli <shanli.alefkhani@gmail.com>
2020-11-01 14:06:02 +0330

    finished :d

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -80,35 +80,54 @@ class Results extends StatefulWidget {
 class _ResultsState extends State<Results> {
   final String searchName;
   final _nextPageThreshold = 3;
-  bool hasNextPage = true;
-  int pageNum = 1;
+  bool hasNextPage;
+  int pageNum;
+  bool _loading;
+  bool _error;
+
+  @override
+  void initState() {
+    super.initState();
+    hasNextPage = true;
+    pageNum = 1;
+    _error = false;
+    _loading = true;
+    fetchBooks();
+  }
 
   _ResultsState(this.searchName);
 
   List<Book> bookArr = new List();
 
-  Future<List> getData() async {
+  /*Future<List> getData() async {
     Dio dio = new Dio();
-    final response =
-        await dio.get("https://api.koobook.app/books/", queryParameters: {
-      "search": searchName,
-      "fields":
-          "url,Title,ISBN,Image,Description,Publisher,Price,Edition,Hashtags,Rate,Authors",
-      "page": pageNum,
-    });
-    //print(response.data['results']);
-    if (response.data['next'] == null) {
-      hasNextPage = false;
-    } else {
-      pageNum++;
-    }
-    if (response.statusCode == 200) {
-      return response.data['results'];
+    try {
+      final response =
+      await dio.get("https://api.koobook.app/books/", queryParameters: {
+        "search": searchName,
+        "fields":
+        "url,Title,ISBN,Image,Description,Publisher,Price,Edition,Hashtags,Rate,Authors",
+        "page": pageNum,
+      });
+      //print(response.data['results']);
+      _loading = false;
+      if (response.data['next'] == null) {
+        hasNextPage = false;
+      } else {
+        pageNum++;
+      }
+      if (response.statusCode == 200) {
+        return response.data['results'];
+      }
+    } catch(e) {
+      _loading = false;
+      _error = true;
     }
     return null;
   }
 
   Future<bool> starting() async {
+    debugPrint("starting");
     List response = await getData();
     for (int i = 0; i < response.length; i++) {
       String authorsName = "";
@@ -126,13 +145,168 @@ class _ResultsState extends State<Results> {
       bookArr.add(book);
     }
     return true;
+  }*/
+
+  Future<void> fetchBooks() async {
+    try {
+      Dio dio = new Dio();
+      final response =
+      await dio.get("https://api.koobook.app/books/", queryParameters: {
+        "search": searchName,
+        "fields":
+        "url,Title,ISBN,Image,Description,Publisher,Price,Edition,Hashtags,Rate,Authors",
+        "page": pageNum,
+      });
+      List fetchedBooks = response.data['results'];
+      setState(() {
+        if (response.data['next'] == null) {
+          hasNextPage = false;
+        } else {
+          pageNum++;
+        }
+        _loading = false;
+        for (int i = 0; i < fetchedBooks.length; i++) {
+          String authorsName = "";
+          for (int j = 0; j < fetchedBooks[i]['Authors'].length; j++) {
+            authorsName += fetchedBooks[i]['Authors'][j]["Name"] + " ";
+          }
+          Book book = new Book(
+              fetchedBooks[i]['url'],
+              fetchedBooks[i]['Title'],
+              fetchedBooks[i]['Image'],
+              fetchedBooks[i]['Description'],
+              fetchedBooks[i]['Publisher'],
+              fetchedBooks[i]['Rate'],
+              authorsName);
+          bookArr.add(book);
+        }
+        return true;
+      });
+    } catch (e) {
+      setState(() {
+        _loading = false;
+        _error = true;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey.shade900,
-      body: Center(
+        backgroundColor: Colors.blueGrey.shade900,
+        body: getBody()
+    );
+  }
+
+  Widget getBody() {
+    debugPrint("getbody ${bookArr.length} $_loading $_error");
+    if (bookArr.isEmpty) {
+      if (_loading) {
+        return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: CircularProgressIndicator(),
+            ));
+      } else if (_error) {
+        return Center(
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  _loading = true;
+                  _error = false;
+                  fetchBooks();
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text("Error while loading photos, tap to try agin"),
+              ),
+            ));
+      }
+    } else {
+      return ListView.builder(
+          itemCount: bookArr.length + (hasNextPage ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (index == bookArr.length - _nextPageThreshold) {
+              fetchBooks();
+            }
+            if (index == bookArr.length) {
+              if (_error) {
+                return Center(
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          _loading = true;
+                          _error = false;
+                          fetchBooks();
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                            "Error while loading photos, tap to try agin"),
+                      ),
+                    ));
+              } else {
+                return Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: CircularProgressIndicator(),
+                    ));
+              }
+            }
+            final Book book = bookArr[index];
+            return Card(
+              elevation: 5,
+              color: Colors.white,
+              child: ListTile(
+                title: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: Text("${book.title}",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.amber.shade700,
+                      )),
+                ),
+                subtitle: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          book.authors,
+                          style:
+                          TextStyle(color: Colors.grey.shade700),
+                        ),
+                        Text(
+                          "${book.publisher}",
+                          style:
+                          TextStyle(color: Colors.grey.shade700),
+                        ),
+                      ]),
+                ),
+                trailing: CircleAvatar(
+                  backgroundColor: Colors.blueGrey.shade900,
+                  backgroundImage: NetworkImage(
+                    book.image,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) =>
+                            DetailPage(book)),
+                  );
+                },
+              ),
+            );
+          });
+    }
+    return Container();
+  }
+
+/*Center(
         child: FutureBuilder(
           future: starting(),
           builder: (context, snapshot) {
@@ -209,9 +383,7 @@ class _ResultsState extends State<Results> {
             }
           },
         ),
-      ),
-    );
-  }
+      ),*/
 }
 
 class DetailPage extends StatelessWidget {
